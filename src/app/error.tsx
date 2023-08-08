580001d5c8ccf8fde7dd65eeb862a5b8382b3ccd
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-08-08 20:34:52 +0700

    add routing files convention

---
+++ src/app/error.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+interface ErrorPageProps {
+  error: Error;
+  reset: () => void;
+}
+
+export default function Error({ error, reset }: ErrorPageProps) {
+  return (
+    <div>
+      <h1>Error 😵</h1>
+      <p>Something went wrong</p>
+      <button type='button' onClick={reset}>
+        Try again
+      </button>
+    </div>
+  );
+}
