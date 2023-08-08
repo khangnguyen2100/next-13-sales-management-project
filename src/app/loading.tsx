ab953cf8b9bcffb4eeb71a0ea19c68bdbdea1cc7
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-08-08 23:00:02 +0700

    export mui  to use in server component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,9 @@
+import { CircularProgress } from '@/components/Mui';
+
 export default function Loading() {
-    return <div>loading...</div>
+  return (
+    <div className='w-full min-h-screen grid place-items-center'>
+      <CircularProgress />
+    </div>
+  );
 }
---
+++ src/components/Mui/index.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+export {
+  Container,
+  Paper,
+  Chip,
+  Divider,
+  CircularProgress,
+  Typography,
+  Skeleton,
+  Breadcrumbs,
+  Menu,
+  MenuItem,
+  Box,
+  Grid,
+} from '@mui/material';
