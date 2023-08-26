f05a561f9e54680569e818f0ee40a133cb95e3f3
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-08-26 11:16:05 +0700

    feat: update home page

---
+++ src/components/home/Banner.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import ButtonLink from '@/components/common/ButtonLink';
+import HeroBanner from '@/components/common/HeroBanner';
+import BannerImg from 'public/images/home/banner.jpg';
+import PlayVideoIcon from 'public/icons/play-video.svg';
+
+const HomeHeroBanner = () => {
+  return (
+    <HeroBanner imageSrc={BannerImg} height='large'>
+      <div className='mb-3 flex items-center gap-x-[7px]'>
+        <div className='h-3 w-3 bg-secondary'></div>
+        <p className='text-lg text-white'>
+          Chào mừng bạn đến với
+          <span className='font-semibold text-secondary'> BeeSmart</span>
+        </p>
+      </div>
+      <h1 className='text-5xl font-bold leading-[1.1] text-white md:text-[60px]'>
+        <span className='block'>Phần mềm</span>
+        <span className='block'>
+          <span className='text-secondary'>Quản lí </span>
+          bán hàng
+        </span>
+        <span className='block'>phổ biến nhất</span>
+      </h1>
+      <div className='mt-9 flex items-center gap-x-8'>
+        <ButtonLink color='secondary' type='button' href='/login'>
+          Dùng Thử Miễn Phí
+        </ButtonLink>
+        <Link
+          className='transition-sm group flex items-center gap-x-3'
+          href={'/'}
+        >
+          <Image
+            src={PlayVideoIcon}
+            width={60}
+            height={60}
+            alt='play icon'
+            className=' transition-sm group-hover:scale-[1.05]'
+          />
+          <div className='flex flex-col'>
+            <p className='text-lg font-medium text-white'>Khám Phá Ngay</p>
+            <div className='transition-sm mt-[-1px] h-[2px] w-0 bg-white group-hover:w-full'></div>
+          </div>
+        </Link>
+      </div>
+    </HeroBanner>
+  );
+};
+
+export default HomeHeroBanner;
---
+++ src/components/home/Solutions.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import Image, { StaticImageData } from 'next/image';
+import Link from 'next/link';
+
+import Icon1 from 'public/icons/home-solution-icon-1.svg';
+import Icon2 from 'public/icons/home-solution-icon-2.svg';
+import Icon3 from 'public/icons/home-solution-icon-3.svg';
+import Icon4 from 'public/icons/home-solution-icon-4.svg';
+import Icon5 from 'public/icons/home-solution-icon-5.svg';
+
+import HeadingWithDot from '../common/HeadingWithDot';
+
+type SolutionItem = {
+  title: string;
+  href: string;
+  icon: StaticImageData;
+};
+const solutionList: SolutionItem[] = [
+  {
+    title: 'Tư vấn Tài chính',
+    href: '/',
+    icon: Icon1,
+  },
+  {
+    title: 'Quản lí Việc kinh doanh',
+    href: '/',
+    icon: Icon2,
+  },
+  {
+    title: 'Consultants & program',
+    href: '/',
+    icon: Icon3,
+  },
+  {
+    title: 'Company Management',
+    href: '/',
+    icon: Icon4,
+  },
+  {
+    title: 'Financial & Insurance jobs',
+    href: '/',
+    icon: Icon5,
+  },
+];
+
+const Solutions = () => {
+  return (
+    <div className='mx-auto mb-16 w-full max-w-large px-4'>
+      <HeadingWithDot variant='secondary' color='black' className='mb-4'>
+        Chúng tôi là chuyên gia trong
+      </HeadingWithDot>
+      <h2 className='mx-auto mb-10 max-w-[815px] text-center text-[40px] mdd:text-3xl mobile:text-2xl'>
+        <span className='block'>Chúng tôi cung cấp giải pháp</span>
+        <span className='block'> chuyên nghiệp cho doanh nghiệp của bạn.</span>
+      </h2>
+      {/* solution list */}
+      <div className='grid grid-cols-3 gap-x-8 gap-y-7 lgd:grid-cols-2 smd:grid-cols-1 '>
+        {solutionList.map((item, i) => {
+          const { icon, title, href } = item;
+          return (
+            <div
+              key={i}
+              className='transition-md group col-span-1 flex min-h-[173px] items-center justify-between bg-section-5 p-[30px] hover:shadow-md mdd:p-5'
+            >
+              {/* content */}
+              <div className='max-w-[170px]'>
+                <h3 className='mb-7 text-2xl'>{title}</h3>
+                <Link
+                  className='text-lg font-semibold text-primary'
+                  href={href}
+                >
+                  Read More
+                </Link>
+              </div>
+              {/* icon */}
+              <div className='flex-center transition-md h-[72px] w-[72px] shrink-0 rounded-full bg-secondary shadow-md group-hover:bg-primary'>
+                <Image
+                  src={icon}
+                  alt={title}
+                  width={42}
+                  height={42}
+                  className='object-cover'
+                />
+              </div>
+            </div>
+          );
+        })}
+        <div className='flex-center transition-md  col-span-1 min-h-[173px] bg-primary hover:shadow-md'>
+          <Link href={'/services'}>
+            <h3 className='text-2xl text-white'>More Services</h3>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Solutions;
