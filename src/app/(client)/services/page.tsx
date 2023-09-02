afe68bd12405c6b82e6ebc2b496aae62564dbd24
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-09-02 21:48:29 +0700

    enhance banner

--- src/app/(client)/services/page.tsx
+++ src/app/(client)/services/page.tsx
@@ -1,16 +1,18 @@
 import ClientLayout from '@/components/Layout/ClientLayout';
-import {
-  ServicesBanner,
-  ServicesOffering,
-  ServicesAdvance,
-} from '@/components/services';
+import { ServicesOffering, ServicesAdvance } from '@/components/services';
 import { Solutions } from '@/components/home';
 import ReviewList from '@/components/review/ReviewList';
-
+import Banner from '@/components/common/Banner';
+import BannerImg from 'public/images/services/banner.jpg';
 export default function Index() {
   return (
     <ClientLayout>
-      <ServicesBanner />
+      <Banner
+        imageSrc={BannerImg}
+        heading='Dịch vụ của chúng tôi'
+        title='Quản lý dịch vụ'
+        description='Chúng tôi thiết kế từng loại dịch vụ cho riêng từng ngành hàng khác nhau, đa dạng dịch vụ. '
+      ></Banner>
       <Solutions full />
       <ServicesOffering />
       <ServicesAdvance />
--- src/components/services/ServicesAdvanceSection.tsx
+++ src/components/services/ServicesAdvanceSection.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-import { CircularProgress } from 'components/Mui';
+import { CircularProgress, Divider } from 'components/Mui';
 import starIcon from 'public/icons/star.svg';
 import vCheckIcon from 'public/icons/v-check-secondary.svg';
 
@@ -42,76 +42,104 @@ const CircularItem = (props: CircularItemProps) => {
     </div>
   );
 };
+
 const ServicesAdvance = () => {
   return (
-    <div className='flex-center container my-24 w-full gap-x-[72px] gap-y-10 lgd:flex-col'>
-      {/* content */}
-      <div className='text-typo-1'>
-        {/* header */}
-        <div className='lg:max-w-[572px]'>
-          <HeadingWithDot color='black' variant='primary' align='start'>
-            Tại sao lại chọn KioViet
-          </HeadingWithDot>
-          <h2 className='mb-6 mt-3 text-3xl font-bold mdd:text-2xl'>
-            Chúng tôi cung cấp giải pháp để phát triển doanh nghiệp của bạn
-          </h2>
-          <p className='mt-2 text-lg text-typo-3'>
-            Leverage agile frameworks to provide a robust synopsis for high
-            level overviews. thinking to further the overall proposition.
-          </p>
-        </div>
+    <div className='container mt-24 w-full'>
+      <div className='flex-center gap-x-[72px] gap-y-10 lgd:flex-col'>
+        {/* content */}
+        <div className='text-typo-1'>
+          {/* header */}
+          <div className='lg:max-w-[572px]'>
+            <HeadingWithDot color='black' variant='primary' align='start'>
+              Tại sao lại chọn KioViet
+            </HeadingWithDot>
+            <h2 className='mb-6 mt-3 text-3xl font-bold mdd:text-2xl'>
+              Chúng tôi cung cấp giải pháp để phát triển doanh nghiệp của bạn
+            </h2>
+            <p className='mt-2 text-lg text-typo-3'>
+              Leverage agile frameworks to provide a robust synopsis for high
+              level overviews. thinking to further the overall proposition.
+            </p>
+          </div>
 
-        {/* check list */}
-        <div className='mt-9 grid grid-cols-2 gap-x-5 gap-y-6  mobile:grid-cols-1'>
-          <div className='col-span-1 flex items-center gap-x-2'>
-            <Image src={vCheckIcon} width={27} height={27} alt='v-check-icon' />
-            <p className='text-lg text-typo-1 '>
-              <span className='font-bold '>2,800+ </span>
-              KH đang hoạt động
-            </p>
-          </div>
-          <div className='col-span-1 flex items-center gap-x-2'>
-            <Image src={vCheckIcon} width={27} height={27} alt='v-check-icon' />
-            <p className='text-lg text-typo-1 '>Kênh hỗ trợ youtube</p>
-          </div>
-          <div className='col-span-1 flex items-center gap-x-2'>
-            <Image src={vCheckIcon} width={27} height={27} alt='v-check-icon' />
-            <p className='text-lg text-typo-1 '>
-              <span className='font-bold'>1,670 + </span>
-              <span className='text-[#FFB342]'>5 </span>
+          {/* check list */}
+          <div className='mt-9 grid grid-cols-2 gap-x-5 gap-y-6  mobile:grid-cols-1'>
+            <div className='col-span-1 flex items-center gap-x-2'>
               <Image
-                src={starIcon}
-                width={17}
-                height={17}
-                alt='star'
-                className='inline-block'
+                src={vCheckIcon}
+                width={27}
+                height={27}
+                alt='v-check-icon'
               />
-              {' đánh giá'}
-            </p>
+              <p className='text-lg text-typo-1 '>
+                <span className='font-bold '>2,800+ </span>
+                KH đang hoạt động
+              </p>
+            </div>
+            <div className='col-span-1 flex items-center gap-x-2'>
+              <Image
+                src={vCheckIcon}
+                width={27}
+                height={27}
+                alt='v-check-icon'
+              />
+              <p className='text-lg text-typo-1 '>Kênh hỗ trợ youtube</p>
+            </div>
+            <div className='col-span-1 flex items-center gap-x-2'>
+              <Image
+                src={vCheckIcon}
+                width={27}
+                height={27}
+                alt='v-check-icon'
+              />
+              <p className='text-lg text-typo-1 '>
+                <span className='font-bold'>1,670 + </span>
+                <span className='text-[#FFB342]'>5 </span>
+                <Image
+                  src={starIcon}
+                  width={17}
+                  height={17}
+                  alt='star'
+                  className='inline-block'
+                />
+                {' đánh giá'}
+              </p>
+            </div>
+            <div className='col-span-1 flex items-center gap-x-2'>
+              <Image
+                src={vCheckIcon}
+                width={27}
+                height={27}
+                alt='v-check-icon'
+              />
+              <p className='text-lg text-typo-1 '>Kênh hỗ trợ youtube</p>
+            </div>
           </div>
-          <div className='col-span-1 flex items-center gap-x-2'>
-            <Image src={vCheckIcon} width={27} height={27} alt='v-check-icon' />
-            <p className='text-lg text-typo-1 '>Kênh hỗ trợ youtube</p>
-          </div>
+          <ButtonLink
+            href='/contact'
+            color='primary'
+            type='button'
+            className='mt-12'
+          >
+            Contact Us
+          </ButtonLink>
         </div>
-        <ButtonLink
-          href='/contact'
-          color='primary'
-          type='button'
-          className='mt-12'
-        >
-          Contact Us
-        </ButtonLink>
+        {/* circular progress */}
+        <div className='grid shrink-0 grid-cols-1 gap-8 lg:max-w-[520px]'>
+          <CircularItem
+            heading='Đã đánh giá & phản hồi'
+            description='Khách hàng'
+            value={95}
+          />
+          <CircularItem
+            heading='Đã hoàn thành'
+            description='Dự án'
+            value={85}
+          />
+        </div>
       </div>
-      {/* circular progress */}
-      <div className='grid shrink-0 grid-cols-1 gap-8 lg:max-w-[520px]'>
-        <CircularItem
-          heading='Đã đánh giá & phản hồi'
-          description='Khách hàng'
-          value={95}
-        />
-        <CircularItem heading='Đã hoàn thành' description='Dự án' value={85} />
-      </div>
+      <Divider className='mt-24' />
     </div>
   );
 };
