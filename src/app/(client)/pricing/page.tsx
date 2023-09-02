ff821d67938169e87280cbeea3880c958806d636
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-09-02 22:35:34 +0700

    pricing option section

--- src/app/(client)/pricing/page.tsx
+++ src/app/(client)/pricing/page.tsx
@@ -1,13 +1,15 @@
 import Banner from '@/components/common/Banner';
+import PricingOptionSection from '@/components/pricing/PricingOptionSection';
 import BannerImage from 'public/images/contact/banner.jpg';
 export default function Index() {
   return (
     <>
       <Banner
         imageSrc={BannerImage}
-        heading='Contact Us'
-        title='Hãy liên hệ với chúng tôi'
+        heading='Pricing Package'
+        title='Affordable Plans For Customers'
       ></Banner>
+      <PricingOptionSection />
     </>
   );
 }
