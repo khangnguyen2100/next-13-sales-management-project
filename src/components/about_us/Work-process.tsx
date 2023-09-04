f7a828997abe3c06f76ef4e290bebdc26fca646c
Longdh <danghoanglong511@gmail.com>
2023-09-04 10:33:32 +0700

    changed about_us page

--- src/components/about_us/Work-process.tsx
+++ src/components/about_us/Work-process.tsx
@@ -38,30 +38,24 @@ const WorkProcessCardList: WorkProcessCard[] = [
 ];
 function WorkProcess() {
   return (
-    <div className='mx-auto mb-24 flex  max-w-large flex-col items-center justify-center'>
+    <div className='mx-auto mb-24 flex max-w-large flex-col items-center justify-center mdd:gap-y-4'>
       <HeadingWithDot variant='secondary' color='black' className='mb-4'>
         Quá trình làm việc
       </HeadingWithDot>
       <h2 className='mx-auto mb-10 max-w-[815px] text-center text-[40px] capitalize mdd:text-3xl mobile:text-2xl'>
         Cách làm việc của chúng tôi
       </h2>
-      <div className='grid grid-cols-12 gap-x-7 '>
+      <div className='grid grid-cols-12 gap-x-7 mdd:gap-y-14'>
         {WorkProcessCardList.map((item, index) => {
           const { title, content, href, icon } = item;
           return (
             <div
-              className='relative col-span-4 flex flex-col gap-y-5 bg-gray-100 px-10 py-14 text-center'
+              className='relative col-span-4 flex flex-col gap-y-5 bg-gray-100 px-10 py-14 text-center mdd:col-span-12 mdd:px-20'
               key={index}
             >
               {/*Icons*/}
-              <div className='flex-center absolute left-[50%] top-[-10%] translate-x-[-50%] translate-y-[-10%]'>
-                <Image
-                  src={icon}
-                  alt={title}
-                  width={72}
-                  height={72}
-                  className='object-cover'
-                />
+              <div className='flex-center absolute left-[50%] top-[-10%] h-[72px] w-[72px] translate-x-[-50%] translate-y-[-10%]'>
+                <Image src={icon} alt={title} fill className='object-cover' />
               </div>
               {/* Title */}
               <h3 className='text-2xl font-semibold'>{title}</h3>
