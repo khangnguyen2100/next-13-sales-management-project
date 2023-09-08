177db9f84ac1c8f45dd01547bb5dda6adc929c14
Longdh <danghoanglong511@gmail.com>
2023-09-08 22:38:41 +0700

    add admin page, admin layout

---
+++ src/components/admin/AdminHeader.tsx
@@ -0,0 +1,89 @@
+import Image from 'next/image';
+import {
+  MdOutlineAccountBalanceWallet,
+  MdOutlineSavings,
+  MdOutlineColorLens,
+  MdOutlineLiveHelp,
+  MdOutlineFeedback,
+  MdOutlineMail,
+  MdOutlineAccountCircle,
+  MdDeliveryDining,
+  MdOutlineShoppingBag,
+  MdLanguage,
+  MdSettings,
+} from 'react-icons/md';
+/* eslint-disable-next-line import/named */
+import { IconType } from 'react-icons';
+import Link from 'next/link';
+
+import Logo from 'public/logo.png';
+type NavbarLinkType = {
+  title: string;
+  linkTo: string;
+};
+const NavbarLinks: NavbarLinkType[] = [
+  { title: 'Tổng quan', linkTo: '#!' },
+  { title: 'Hàng hóa', linkTo: '#!' },
+  { title: 'Giao dịch', linkTo: '#!' },
+  { title: 'Đối tác', linkTo: '#!' },
+  { title: 'Nhân viên', linkTo: '#!' },
+  { title: 'Sổ quỹ', linkTo: '#!' },
+  { title: 'Báo cáo', linkTo: '#!' },
+];
+type TopHeaderLinksType = {
+  icon: IconType;
+  title?: string;
+  linkTo: string;
+};
+const TopHeaderLinks: TopHeaderLinksType[] = [
+  { icon: MdOutlineShoppingBag, title: 'Bán online', linkTo: '#!' },
+  { icon: MdDeliveryDining, title: 'Giao hàng', linkTo: '#!' },
+  { icon: MdLanguage, title: 'Website', linkTo: '#!' },
+  { icon: MdOutlineAccountBalanceWallet, title: 'Tài chính', linkTo: '#!' },
+  { icon: MdOutlineSavings, title: 'Nguồn hàng giá tốt', linkTo: '#!' },
+  { icon: MdOutlineColorLens, title: 'Chủ đề', linkTo: '#!' },
+  { icon: MdOutlineLiveHelp, title: 'Hỗ trợ', linkTo: '#!' },
+  { icon: MdOutlineFeedback, title: 'Góp ý', linkTo: '#!' },
+  { icon: MdOutlineMail, linkTo: '#!' },
+  { icon: MdSettings, linkTo: '#!' },
+  { icon: MdOutlineAccountCircle, title: '0335702367', linkTo: '#!' },
+];
+function AdminHeader() {
+  return (
+    <>
+      <div className='flex items-center justify-between px-4 py-5 xld:px-0'>
+        <div>
+          <Image src={Logo} alt='Beesmart' className='h-8 w-auto'></Image>
+        </div>
+        <div></div>
+        <div className='flex gap-x-3'>
+          {TopHeaderLinks.map((item, index) => {
+            return (
+              <div key={index} className='flex items-center gap-x-1'>
+                <item.icon size={18}></item.icon>
+                <Link href={item.linkTo} className='text-xs'>
+                  {item.title}
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className='flex bg-[#0090DA] xld:px-0'>
+        {NavbarLinks.map((item, index) => {
+          return (
+            <Link
+              href={item.linkTo}
+              key={index}
+              className='px-5 py-3 text-white hover:bg-[#0078b6]'
+            >
+              {item.title}
+            </Link>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default AdminHeader;
---
+++ src/components/admin/AdminSideBar.tsx
@@ -0,0 +1,41 @@
+function AdminSideBar() {
+  return (
+    <div className='flex w-fit flex-col gap-y-5 self-end rounded-sm px-5 shadow-2xl'>
+      <h3>Các hoạt động gần đây</h3>
+      <div className='flex max-w-[300px] items-center justify-between gap-x-8'>
+        <span>icon</span>
+        <p>Trần Tiến Anh vừa bán đơn hàng với giá trị 540,000</p>
+      </div>
+      <div className='flex max-w-[300px] items-center justify-between gap-x-8'>
+        <span>icon</span>
+        <p>Trần Tiến Anh vừa bán đơn hàng với giá trị 540,000</p>
+      </div>
+      <div className='flex max-w-[300px] items-center justify-between gap-x-8'>
+        <span>icon</span>
+        <p>Trần Tiến Anh vừa bán đơn hàng với giá trị 540,000</p>
+      </div>
+      <div className='flex max-w-[300px] items-center justify-between gap-x-8'>
+        <span>icon</span>
+        <p>Trần Tiến Anh vừa bán đơn hàng với giá trị 540,000</p>
+      </div>
+      <div className='flex max-w-[300px] items-center justify-between gap-x-8'>
+        <span>icon</span>
+        <p>Trần Tiến Anh vừa bán đơn hàng với giá trị 540,000</p>
+      </div>
+      <div className='flex max-w-[300px] items-center justify-between gap-x-8'>
+        <span>icon</span>
+        <p>Trần Tiến Anh vừa bán đơn hàng với giá trị 540,000</p>
+      </div>
+      <div className='flex max-w-[300px] items-center justify-between gap-x-8'>
+        <span>icon</span>
+        <p>Trần Tiến Anh vừa bán đơn hàng với giá trị 540,000</p>
+      </div>
+      <div className='flex max-w-[300px] items-center justify-between gap-x-8'>
+        <span>icon</span>
+        <p>Trần Tiến Anh vừa bán đơn hàng với giá trị 540,000</p>
+      </div>
+    </div>
+  );
+}
+
+export default AdminSideBar;
