5f2c0b3e034597737668616e85284d62baa66ac0
Longdh <danghoanglong511@gmail.com>
2023-11-24 15:54:10 +0700

    fix blog detail page

--- src/app/(client)/blog/[slug]/page.tsx
+++ src/app/(client)/blog/[slug]/page.tsx
@@ -1,19 +1,26 @@
 import { RecentNewType } from '@/components/blog/RecentNews';
 
-export async function getNew(slug: string) {
-  // Fetch data from external API
-  const res = await fetch(`https://admin.beesmart.io.vn/api/tin/${slug}`);
-  const data = await res.json();
-  return data;
-}
 export default async function BlogDetail({
   params,
 }: {
   params: { slug: string };
 }) {
-  try {
-    const data = await getNew(params.slug);
-    const blogItem: RecentNewType = data.tintuc;
-    return <div>{blogItem.noiDung}</div>;
-  } catch (error) {}
+  const data = await getNew(params.slug);
+  const blogItem: RecentNewType = data.data;
+  return (
+    <div className='mx-auto mb-24 mt-10 grid max-w-7xl grid-cols-12 flex-col gap-x-10'>
+      <div
+        dangerouslySetInnerHTML={{ __html: blogItem.noiDung }}
+        className='blogContainer col-span-8'
+      ></div>
+      <div className='col-span-4 flex flex-col'></div>
+    </div>
+  );
+}
+
+ async function getNew(slug: string) {
+  // Fetch data from external API
+  const res = await fetch(`https://admin.beesmart.io.vn/api/tin/${slug}`);
+  const data = await res.json();
+  return data;
 }
