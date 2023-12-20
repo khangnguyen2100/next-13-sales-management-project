6f1277855cbf0b78f9aef3afa42024f9c5cef34a
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-12-20 12:52:57 +0700

    add back btn

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,7 @@
-import Image from 'next/image';
 import { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FaArrowLeftLong } from 'react-icons/fa6';
 
 import SignUpForm from '@/components/sign-up/SignUpForm';
 import ImageSrc from 'public/images/auth/sign-up-banner.jpg';
@@ -20,6 +22,11 @@ function Page() {
           className='object-cover'
           placeholder='blur'
         />
+        <div className='absolute left-6 top-8 z-10 cursor-pointer'>
+          <Link href='/'>
+            <FaArrowLeftLong className='text-white' color='#fff' size={24} />
+          </Link>
+        </div>
         <div className='absolute inset-y-0 left-0 h-full w-full bg-section-2/80 backdrop-blur-[3.5px]'>
           <h2 className='abs-center top-1/3 w-max text-center text-5xl leading-[1.5] text-white mdd:text-4xl'>
             <span className='block'>Quản lý dễ dàng</span>
